9d636f33ce5b8dfa111d168a5fdbc84e889ba8de
arankin <arankin@ualberta.ca>
2017-10-18 19:06:18 -0600

    added friend tests for real this time

---
+++ app/src/main/java/com/example/cmput301f17t30/habitrabbit/Friend.java
@@ -0,0 +1,21 @@
+package com.example.cmput301f17t30.habitrabbit;
+
+/**
+ * Created by arankin on 10/18/17.
+ */
+
+public class Friend {
+    public Integer id;
+
+    public Friend(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
---
+++ app/src/test/java/com/example/cmput301f17t30/habitrabbit/TestFriend.java
@@ -0,0 +1,29 @@
+package com.example.cmput301f17t30.habitrabbit;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by arankin on 10/18/17.
+ */
+
+public class TestFriend extends ActivityInstrumentationTestCase2 {
+
+    public TestFriend() {
+        super(Friend.class);
+    }
+
+    public void testSetId(){
+        Integer ID = 5;
+        Friend friend = new Friend(ID);
+        assertEquals(ID, friend.getId());
+    }
+
+    public void testGetId(){
+        Integer ID = 5;
+        Friend friend = new Friend(ID);
+        Integer newID = 7;
+        friend.setId(newID);
+        assertEquals(newID, friend.getId());
+
+    }
+}
