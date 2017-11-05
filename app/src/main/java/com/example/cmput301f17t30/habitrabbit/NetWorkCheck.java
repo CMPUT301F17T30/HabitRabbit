2f7f4c6a70992fffc7919373177fddea6e64954e
Katzesama <31713290+Katzesama@users.noreply.github.com>
2017-11-04 19:10:05 -0600

    Update NetWorkCheck.java

--- app/src/main/java/com/example/cmput301f17t30/habitrabbit/NetWorkCheck.java
+++ app/src/main/java/com/example/cmput301f17t30/habitrabbit/NetWorkCheck.java
@@ -1,9 +1,18 @@
 package com.example.cmput301f17t30.habitrabbit;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
 /**
  * Created by Irissama on 2017-11-04.
  */
 
 public class NetWorkCheck {
-
+    public boolean isNetWorkAvaliable(Context context){
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
 }
