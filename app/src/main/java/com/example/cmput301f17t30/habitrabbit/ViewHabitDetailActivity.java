7306515734bf3ff532fb07d90af06f20641c4b12
jrforest <jrforest@ualberta.ca>
2017-11-09 18:43:54 -0700

    ViewHabitDetail added, needs body

---
+++ app/src/main/java/com/example/cmput301f17t30/habitrabbit/ViewHabitDetailActivity.java
@@ -0,0 +1,17 @@
+package com.example.cmput301f17t30.habitrabbit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewHabitDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_habit_detail);
+
+        
+
+
+    }
+}
