1e89c3e4657219f093f53cbe7d52adb92fbe3a97
a-rankin <arankin@ualberta.ca>
2017-11-09 18:32:03 -0700

    added javadoc to habit class
    like title says

--- app/src/main/java/com/example/cmput301f17t30/habitrabbit/Habit.java
+++ app/src/main/java/com/example/cmput301f17t30/habitrabbit/Habit.java
@@ -13,7 +13,12 @@ public class Habit {
     private Integer timesFailed;
     private Integer timesCompleted;
 
-
+    /**
+     *
+     * @param title A unique identifier string for a type of habit
+     * @param reason optional string, the reason for doing the habit
+     * @param days the days of each week that the habit will be due
+     */
     public Habit(String title, String reason, ArrayList<Boolean> days) {
         Title = title;
         Reason = reason;
@@ -22,7 +27,11 @@ public class Habit {
         timesFailed =0;
     }
 
-
+    /**
+     *
+     * @param title A unique identifier string for a type of habit
+     * @param days the days of each week that the habit will be due
+     */
     public Habit(String title, ArrayList<Boolean> days) {
         Title = title;
         this.days = days;
@@ -30,77 +39,138 @@ public class Habit {
         timesFailed = 0;
     }
 
+    /**
+     *
+     * @return the title of the habit
+     */
     public String getTitle() {
 
         return Title;
     }
 
+    /**
+     *
+     * @param title A unique identifier string for a type of habit
+     */
     public void setTitle(String title) {
 
         Title = title;
     }
 
+    /**
+     *
+     * @return the reason for doing the habit
+     */
     public String getReason() {
 
         return Reason;
     }
 
+    /**
+     *
+     * @param reason the reason for doing the habit
+     */
     public void setReason(String reason) {
 
         Reason = reason;
     }
 
+    /**
+     *
+     * @return the days of each week that the habit will be due
+     */
     public ArrayList<Boolean> getDays() {
 
         return days;
     }
 
+    /**
+     *
+     * @param days the days of each week that the habit will be due
+     */
     public void setDays(ArrayList<Boolean> days) {
 
         this.days = days;
     }
 
+    /**
+     *
+     * @return whether not not the habit is due to be completed today
+     */
+    //TODO implement this method
     public Boolean isDueToday(){
 
         return Boolean.FALSE;
     }
 
+    /**
+     *
+     * @return percentage of possible due dates that the habit was completed
+     */
     public double getPercentCompletion(){
 
         return (double) ((timesFailed+timesCompleted)/timesCompleted);
     }
 
+    /**
+     *
+     * @return number of times the user has failed to complete this habit
+     */
     public Integer getTimesFailed() {
         return timesFailed;
     }
 
+    /**
+     *
+     * @param timesFailed number of times the user has failed to complete this habit
+     */
     public void setTimesFailed(Integer timesFailed) {
         this.timesFailed = timesFailed;
     }
 
+    /**
+     *
+     * @return The number of times the user has completed this habit
+     */
     public Integer getTimesCompleted() {
         return timesCompleted;
     }
 
+    /**
+     *
+     * @param timesCompleted Number of times user has completed this habit
+     */
     public void setTimesCompleted(Integer timesCompleted) {
         this.timesCompleted = timesCompleted;
     }
 
+    /**
+     *  Increments the times failed counter
+     */
     public void incrementTimesFailed(){
         this.timesFailed +=1;
 
     }
 
+    /**
+     * Decrements the times failed counter
+     */
     public void decrementTimesFailed(){
         this.timesFailed -=1;
 
     }
 
+    /**
+     * Increments the times completed counter
+     */
     public void incrementTimesCompleted(){
         this.timesCompleted +=1;
 
     }
 
+    /**
+     * Decrements the times completed counter
+     */
     public void decrementTimesCompleted(){
         this.timesCompleted -=1;
 
