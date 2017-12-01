7562cc59bdb721397112b6272c62c47232c25c2c
adam <arankin@ualberta.ca>
2017-12-01 13:38:21 -0700

    added friend request class

---
+++ app/src/main/java/com/example/cmput301f17t30/habitrabbit/FriendRequest.java
@@ -0,0 +1,79 @@
+/*
+ *     <HabitRabbit- A habit tracking app.>
+ *     Copyright (C) <2017>
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.example.cmput301f17t30.habitrabbit;
+
+import static com.example.cmput301f17t30.habitrabbit.MainActivity.userController;
+
+/**
+ * Model class representing a friend request.
+ */
+
+public class FriendRequest {
+    private String sender;
+    private String reciever;
+    private Boolean accepted;
+
+    public FriendRequest(String sender, String reciever){
+        this.sender = sender;
+        this.reciever = reciever;
+        this.accepted = null;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getReciever() {
+        return reciever;
+    }
+
+    public void setReciever(String reciever) {
+        this.reciever = reciever;
+    }
+
+    public Boolean getAccepted() {
+        return accepted;
+    }
+
+    public void setAccepted(Boolean accepted) {
+        if (accepted == Boolean.TRUE) {
+            this.accepted = Boolean.TRUE;
+        } else if (accepted == Boolean.FALSE) {
+            this.accepted = Boolean.FALSE;
+            //delete request
+        }
+    }
+
+    public void checkRequestStatus(){
+        if (getAccepted() == Boolean.TRUE){
+            //reciever has accepted, add the reciever to friends list
+            userController.addFriend(new User(reciever));
+        }
+        else if (getAccepted() == Boolean.FALSE){
+            // user got rejected
+        }
+        else {
+            // friend has not responded to request yet
+        }
+    }
+}
