a8c3c3a3862a27e71614ed69b76ee667bc3a3c4d
adam <arankin@ualberta.ca>
2017-12-01 13:28:21 -0700

    added ability to select profile picture for user

--- app/src/main/java/com/example/cmput301f17t30/habitrabbit/UserProfileActivity.java
+++ app/src/main/java/com/example/cmput301f17t30/habitrabbit/UserProfileActivity.java
@@ -18,17 +18,37 @@
 
 package com.example.cmput301f17t30.habitrabbit;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.preference.PreferenceManager;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ViewFlipper;
+
+import java.util.Locale;
 
 import static com.example.cmput301f17t30.habitrabbit.MainActivity.achievementController;
 import static com.example.cmput301f17t30.habitrabbit.MainActivity.userController;
@@ -39,11 +59,24 @@ public class UserProfileActivity extends AppCompatActivity {
     private LinearLayoutManager linearLayoutManager;
     private AchievementLayoutAdapter adapter;
 
+    private Bitmap profileImage;
+    private String imageDecode;
+
+    private TextView usernameText;
+    private ImageView profilePic;
+
+    private ImageController imageController = new ImageController();
+    SharedPreferences sharedPreferences;
+
+    private final Integer IMAGE_REQUEST_CODE = 0;
+    private final Integer IMAGE_RESULT = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         final ViewFlipper viewFlipper = (ViewFlipper) findViewById(R.id.profile_viewflipper);
 
         recyclerView = (RecyclerView) findViewById(R.id.achievementsLayout);
@@ -52,8 +85,22 @@ public class UserProfileActivity extends AppCompatActivity {
         adapter = new AchievementLayoutAdapter(achievementController.getAchievements(), this);
         recyclerView.setAdapter(adapter);
 
-        TextView usernameText = (TextView) findViewById(R.id.username_text);
+        usernameText = (TextView) findViewById(R.id.username_text);
         usernameText.setText(userController.getUsername());
+
+        profilePic = (ImageView) findViewById(R.id.profile_picture);
+        if (userController.getProfilePic() != null){
+            profilePic.setImageBitmap(userController.getProfilePic());
+        }
+
+        profilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, IMAGE_RESULT);
+            }
+        });
 
         Button achievementsButton = (Button) findViewById(R.id.achievements_button);
         achievementsButton.setOnClickListener(new View.OnClickListener() {
@@ -77,6 +124,48 @@ public class UserProfileActivity extends AppCompatActivity {
             public void onClick(View view) {
                 viewFlipper.setDisplayedChild(3);
             }
+        });
+
+        Spinner languageSpinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.languages_array, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        languageSpinner.setAdapter(adapter);
+        languageSpinner.setSelection(0, false);
+
+        languageSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                String language = "en";
+                Locale locale;
+
+                Configuration config = new Configuration();
+                if (position == 0){
+                    language = "en";
+                }
+                else if (position == 1){
+                    language = "ru";
+
+                }
+                //Resources englishRes = getApplicationContext().getResources();
+                //DisplayMetrics dm2 = englishRes.getDisplayMetrics();
+                //android.content.res.Configuration conf1 = englishRes.getConfiguration();
+                //conf1.locale = new Locale(language);
+                //englishRes.updateConfiguration(conf1, dm2);
+                locale = new Locale(language);
+                Locale.setDefault(locale);
+                config.locale = locale;
+                getResources().updateConfiguration(config, null);
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // do nothing
+            }
+
         });
     }
 
@@ -102,10 +191,83 @@ public class UserProfileActivity extends AppCompatActivity {
 
         if (id == R.id.logout_button) {
             Intent logout = new Intent(UserProfileActivity.this, LoginActivity.class);
-            getApplicationContext().getSharedPreferences("YOUR_PREFS", 0).edit().clear().apply();
+            SharedPreferences mySPrefs =PreferenceManager.getDefaultSharedPreferences(this);
+            SharedPreferences.Editor editor = mySPrefs.edit();
+            editor.remove("username").apply();
             startActivity(logout);
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void checkPermission(int requestType) {
+
+        final String permission = Manifest.permission.READ_EXTERNAL_STORAGE;
+        // no permission, ask permission
+        if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
+                ActivityCompat.requestPermissions(UserProfileActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, IMAGE_REQUEST_CODE);
+
+            } else {
+                ActivityCompat.requestPermissions(UserProfileActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, IMAGE_REQUEST_CODE);
+
+            }
+        } else {
+            // has permission, get the image
+            profileImage = imageController.decodeFile(imageDecode);
+            profilePic.setImageBitmap(profileImage);
+            userController.setProfilePicture(profileImage);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+
+
+
+        // If request is cancelled, the result arrays are empty.
+        if (grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Bitmap newProfilePic = imageController.decodeFile(imageDecode);
+            profilePic.setImageBitmap(newProfilePic);
+            // permission was granted
+
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            Toast.makeText(UserProfileActivity.this, "Permission needed to access photo gallery.", Toast.LENGTH_SHORT).show();
+            // permission denied
+        }
+        return;
+    }
+
+
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+
+            if (requestCode == IMAGE_RESULT && resultCode == RESULT_OK && null != data) {
+                Uri URI = data.getData();
+                String[] FILE = { MediaStore.Images.Media.DATA };
+                Cursor cursor = getContentResolver().query(URI, FILE, null, null, null);
+                cursor.moveToFirst();
+                int columnIndex = cursor.getColumnIndex(FILE[0]);
+                imageDecode = cursor.getString(columnIndex);
+                cursor.close();
+                // check permission to get image
+                checkPermission(IMAGE_REQUEST_CODE);
+
+            }
+        } catch (Exception e) {
+            // unable to get the path
+            Toast.makeText(this, "Please try again", Toast.LENGTH_LONG)
+                    .show();
+        }
+
     }
 
 
