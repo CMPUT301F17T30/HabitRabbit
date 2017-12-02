9574836741c616e9fbd2e9e22994ae6017298cce
xgao1 <31713263+xgao1@users.noreply.github.com>
2017-12-01 17:42:40 -0700

    intent test for login

---
+++ app/src/androidTest/java/com/example/cmput301f17t30/habitrabbit/LoginTest.java
@@ -0,0 +1,40 @@
+package com.example.cmput301f17t30.habitrabbit;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.robotium.solo.Solo;
+
+import static com.example.cmput301f17t30.habitrabbit.MainActivity.habitController;
+import static com.example.cmput301f17t30.habitrabbit.MainActivity.userController;
+
+/**
+ * Created by gaoxin on 2017/12/1.
+ */
+
+public class LoginTest extends ActivityInstrumentationTestCase2<LoginActivity> {
+    private Solo solo;
+
+    public LoginTest(){
+        super(com.example.cmput301f17t30.habitrabbit.LoginActivity.class);
+    }
+
+    public void setUp()throws Exception{
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+
+    public void testLogin(){
+        solo.assertCurrentActivity("Not User!", LoginActivity.class);
+        solo.enterText((EditText) solo.getView(R.id.userName), "New User");
+        solo.clickOnView((Button) solo.getView(R.id.loginButton));
+        solo.sleep(2000);
+        assertTrue(userController.getUsername().equals("New User"));
+        solo.clickOnView((Button) solo.getView(R.id.loginButton));
+    }
+
+    public void tearDown() throws Exception {
+        solo.finishOpenedActivities();
+    }
+
+}
